2e381673cc54cbe872fad0500877761267cb8476
Jon <jonfernal@gmail.com>
2023-09-21 16:17:31 +0200

    V1.0 del archivo cards. Ordenado y comentado.

--- cards.py
+++ cards.py
@@ -1,49 +1,62 @@
+# Clase Cards se encarga de gestionar las cartas del juego.
+# Creado por JonFdz
+# GitHub: https://www.github.com/jonfdz
+# Versión: 1.0
+
 from tkinter import *
 
+class Cards:
+    """Clase Cards que se encargara de gestionar todas las funcionalidades de las cartas."""
+    def __init__(self) -> None:
+        self.back_img = PhotoImage(file="images/back_card.png")
+        self.card_front_img = PhotoImage(file="images/ace_of_hearts.png")
+        self.cards_list = self.list_cards(0, 8, self.back_img)
+        self.count = 0
 
-def choice_card(index):
-    global count
-    if count < 2:
-        count += 1
-        return cards[index].configure(image=card_front_img, highlightthickness=5, highlightbackground="red")
-    elif count < 4:
-        count += 1
-        return cards[index].configure(image=card_front_img, highlightthickness=5, highlightbackground="blue")
-    elif count < 6:
-        count += 1
-        return cards[index].configure(image=card_front_img, highlightthickness=5, highlightbackground="yellow")
-    else:
-        count += 1
-        return cards[index].configure(image=card_front_img, highlightthickness=5, highlightbackground="green")
+    def choice_card(self, index):
+        """Funcion que gestiona la seleccion de la carta. Determina que carta se pulsa y el color que la sobresalta para formar las parejas."""
+        if self.count < 2:
+            self.count += 1
+            return self.cards_list[index].configure(
+                image=self.card_front_img, highlightthickness=5, highlightbackground="red"
+            )
+        elif self.count < 4:
+            self.count += 1
+            return self.cards_list[index].configure(
+                image=self.card_front_img, highlightthickness=5, highlightbackground="blue"
+            )
+        elif self.count < 6:
+            self.count += 1
+            return self.cards_list[index].configure(
+                image=self.card_front_img, highlightthickness=5, highlightbackground="yellow"
+            )
+        else:
+            self.count += 1
+            return self.cards_list[index].configure(
+                image=self.card_front_img, highlightthickness=5, highlightbackground="green"
+            )
 
-def generate_card(index, image):
-    return Button(image=image, highlightthickness=5, highlightbackground="#B1DDC6",
-                  command=lambda: choice_card(index))
+    def generate_card(self, index, image):
+        """Funcion que genera un boton que representa una carta."""
+        return Button(
+            image=image,
+            highlightthickness=5,
+            highlightbackground="#B1DDC6",
+            command=lambda: self.choice_card(index),
+        )
 
-
-# Constants
-BACKGROUND_COLOR = "#B1DDC6"
-
-# UI
-
-window = Tk()
-window.title("Memory")
-window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-
-card_back_img = PhotoImage(file="images/back_card.png")
-card_front_img = PhotoImage(file="images/ace_of_hearts.png")
-# canvas = Canvas(width=800, height=600, bg=BACKGROUND_COLOR, highlightthickness=0)
-# canvas.grid(row=1, column=4, columnspan=4)
-
-cards = []
-count = 0
-for number in range(0, 8):
-    if number < 4:
-        cards.append(generate_card(number, card_back_img))
-        cards[number].grid(row=0, column=number, rowspan=2, pady=20, padx=20)
-    else:
-        cards.append(generate_card(number, card_back_img))
-        cards[number].grid(row=3, column=number - 4, rowspan=2, pady=20, padx=20)
-
-
-window.mainloop()
+    def list_cards(self, x, y, image):
+        """Funcion que genera una lista de botones que representa las cartas."""
+        list_cards = []
+        for number in range(x, y):
+            if number < 4:
+                list_cards.append(self.generate_card(number, image))
+                list_cards[number].grid(
+                    row=0, column=number, rowspan=2, pady=20, padx=20
+                )
+            else:
+                list_cards.append(self.generate_card(number, image))
+                list_cards[number].grid(
+                    row=3, column=number - 4, rowspan=2, pady=20, padx=20
+                )
+        return list_cards
