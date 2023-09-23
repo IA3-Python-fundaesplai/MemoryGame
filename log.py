8e0c4f0e39c2ff1f1b29d5798fdbb525e2818348
Aitorias <aitordediego23@gmail.com>
2023-09-23 12:55:25 +0200

    Añadir módulo logging para mejores logs

--- log.py
+++ log.py
@@ -1,23 +1,28 @@
 # Clase Log para guardar el registro de puntuaciones del usuario.
 # Creado por Aitor
 # GitHub: https://www.github.com/aitorias
-# Fecha: 2023/09/20
+# Fecha creación: 2023/09/20
+# Última actualización: 2023/09/23
 # Versión: 1.0
 
 import datetime
 import locale
+import logging
 import os
 import pwd
 
 
 class Log:
-    def __init__(self, language='en_US'):
+    def __init__(self):
         """
         Constructor de la clase Log
         """
         user = self.get_player_username()
         self.user = user
-        self.language = language
+        self.language = locale.getdefaultlocale()[0]
+        self.log_file = f'{self.user}-log.log'
+        logging.basicConfig(filename=self.log_file, level=logging.INFO,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
 
     def __str__(self):
         """
@@ -54,22 +59,26 @@ class Log:
             else:  # Formato de fecha para cualquier otro idioma
                 date_format = '%A %d %B %Y'
             locale.setlocale(locale.LC_TIME, self.language)
-        except locale.Error:
-            pass
+        except locale.Error as error:
+            logging.error(f'Locale error occurred: {error}')
         return datetime.datetime.now().strftime(date_format)
 
     def generate_file(self):
         """
         Función que genera el archivo donde se guardan las puntuaciones del usuario
         """
-        # Instanciamos el nombre del fichero con el nombre del usuario actual
-        file = f'user-{self.user}.log'
         # Instanciamos la fecha actual con su respectivo formato
         date = self.get_actual_date()
 
         # Abrimos el archivo y actualizamos el contenido
-        with open(file, 'w') as file:
-            file.write(f'Jugador: {self.user} | conectado: {date}')
+        with open(self.log_file, 'a') as file:
+            logging.info(f'Log file generated for {self.user} on {date}')
+
+    def log(self, message):
+        """
+        Función para guardar los logs
+        """
+        logging.info(message)
 
 
 # Instanciamos el objeto de la clase Log
