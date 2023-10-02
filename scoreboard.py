4884b2cebdd662c605325d262e1f6f083b9833d8
Jon <jonfernal@gmail.com>
2023-10-02 09:39:48 +0200

    Arrgleado error del merge

--- scoreboard.py
+++ scoreboard.py
@@ -92,19 +92,3 @@ class Scoreboard:
             self.save_scores()
         except Exception as error:
             logging.error(f'Error while adding score: {error}')
-
-class Scoreboard:
-
-    def __init__(self, name) -> None:
-        self.name = name
-        self.score = 0
-        self.high_score = 0
-
-    def update_score(self):
-        self.score += 1
-
-    def save_score(self):
-        pass
-
-    def top_5(self, file):
-        pass
